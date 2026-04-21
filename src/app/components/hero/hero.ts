abacebe47d8e30d8324abb60b99a061247857c7d
HigorMauricio <higormauriciosilva@gmail.com>
2026-04-21 15:01:29 -0300

    feat: trocar imagem de fundo do hero

--- src/app/components/hero/hero.ts
+++ src/app/components/hero/hero.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Slide } from '../../models/slide';
 
 @Component({
   selector: 'app-hero',
@@ -11,6 +12,36 @@ export class Hero {
     const elemento = document.getElementById(idSecao);
     if(elemento){
       elemento.scrollIntoView({behavior: 'smooth', block: 'start'});
+    }
+  }
+
+  slides: Slide[] = [
+    {
+      image: 'background/tasha-tracie-background.jpg'
+    },
+    {
+      image: 'background/kendrick-background.jpg'
+    },
+    {
+      image: 'background/djavan-background.jpg'
+    }
+  ]
+
+  slideAtual = 0;
+
+  proximoSlide(): void {
+    if(this.slideAtual === this.slides.length -1){
+      this.slideAtual = 0;
+    }else{
+      this.slideAtual++;
+    }
+  }
+
+  slideAnterior(): void {
+    if(this.slideAtual === 0){
+      this.slideAtual = this.slides.length -1;
+    }else{
+      this.slideAtual--;
     }
   }
 }
