590f8d4b9aa3017f8b89a9737626fbe04b4b8e4c
HigorMauricio <higormauriciosilva@gmail.com>
2026-04-23 13:28:27 -0300

    feat: leitura dos dados vindo do json

--- src/app/components/album-gallery/album-gallery.ts
+++ src/app/components/album-gallery/album-gallery.ts
@@ -1,6 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, signal } from '@angular/core';
 import { Album } from '../../models/album';
 import { AlbumCard } from '../album-card/album-card';
+import { AlbumService } from '../../services/album-service';
 
 @Component({
   selector: 'app-album-gallery',
@@ -8,6 +9,15 @@ import { AlbumCard } from '../album-card/album-card';
   templateUrl: './album-gallery.html',
   styleUrl: './album-gallery.css',
 })
-export class AlbumGallery {
+export class AlbumGallery implements OnInit {
+  albums = signal<Album[]>([]);
 
+  constructor(private album_service: AlbumService) {}
+
+  ngOnInit(): void {
+    this.album_service.getAlbums().subscribe({
+      next: (data) => this.albums.set(data),
+      error: (err) => console.error('Erro ao carregar álbuns', err)
+    })
+  }
 }
